e1eac25c4e9e30510ad870110ed09667b07bd38e
Jahidul Islam <me@xahidex.com>
2023-06-21 11:13:04 +0600

    update mdx.tsx

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useMDXComponent } from 'next-contentlayer/hooks';
-import Tweet from './tweet';
+import { Tweet } from "react-tweet"
 
 const CustomLink = (props) => {
   const href = props.href;
